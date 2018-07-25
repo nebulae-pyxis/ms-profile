1a93dfdac7dca54ad043ac163fbcd51e818f5860
juansmolano <juansmolano@gmail.com>
2018-07-25 16:19:13 -0500

    Set as default page

--- frontend/emi/profile/profile.service.ts
+++ frontend/emi/profile/profile.service.ts
@@ -8,7 +8,6 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class ProfileService implements Resolve<any>
 {
-    
     timeline: any;
     about: any;
     photosVideos: any;
